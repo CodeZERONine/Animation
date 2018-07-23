54fac1e1fd65762e17d2de951818d3e353ab4267
CodeZERONine <akshanshssj5@gmail.com>
2018-07-23 10:45:44 +0530

    6

---
+++ app/src/main/java/com/ebglobalventures/animation/Empty.java
@@ -0,0 +1,13 @@
+package com.ebglobalventures.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Empty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
